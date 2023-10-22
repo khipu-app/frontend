7fdedff30bbc5ce59fdb21539246e0764d0f6b5d
Igor Korovchenko <igsekor@gmail.com>
2023-10-22 23:53:24 +0300

    Добавляет менеджер состояний в проект

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp, defineComponent } from 'vue'
+import { createApp } from 'vue'
 
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import stateManager from './state-manager'
 
 createApp(App)
   .use(router)
+  .use(stateManager)
   .mount('#khipu-app')
---
+++ src/state-manager.js
@@ -0,0 +1,5 @@
+import { createPinia } from 'pinia'
+
+const stateManager = createPinia()
+
+export default stateManager
