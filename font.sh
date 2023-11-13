6b7bc44f97a3b220eb31e6a75cd4919e2b9f3a3d
Igor Korovchenko <igsekor@gmail.com>
2023-11-14 00:45:52 +0300

    Добавляет генерацию настройки стиля

--- font.sh
+++ font.sh
@@ -1,23 +1,25 @@
 # Основной скрипт для генерации файлов шрифтов
 GENERATE_FONT() {
-  rm -f $(echo "$OUTPUTPATH/../index.css")
+  rm -f "$OUTPUTPATH/../index.css"
   for i in "${!SUBSETNAMES[@]}"; do
     for j in "${!FORMATS[@]}"; do
       for k in "${!FILENAMES[@]}"; do
         INPUT="$INPUTPATH/${FILENAMES[k]}-subset.${FORMATS[j]}"
         OUTPUT="$OUTPUTPATH/${FILENAMES[k]}-${SUBSETNAMES[i]}.${FORMATS[j]}"
-        REPLACE=$(echo "s/${INPUT//\//\\/}/${OUTPUT//\//\\/}/")
-        REPLACE=$(echo "${REPLACE//\.\\\//}")
-        REPLACE=$(echo "${REPLACE//\./\\\.}")
-        REPLACE=$(echo "${REPLACE//src\\\//\/}")
-        REPLACE=$(echo "${REPLACE//service\//assets\/}")
+        FONT_FAMILY="s/src/font\-family:\ ${FILENAMES[k]//\-*/};\n\ \ src/"
+        FONT_STYLE="s/}/\ \ font-style: Regular;\n\ \ font-weight: ${FILENAMES[k]//*\-/};\n\ \ font-display: fallback;\n}/"
+        REPLACE="s/${INPUT//\//\\/}/${OUTPUT//\//\\/}/"
+        REPLACE="${REPLACE//\.\\\//}"
+        REPLACE="${REPLACE//\./\\\.}"
+        REPLACE="${REPLACE//src\\\//\/}"
+        REPLACE="${REPLACE//service\//assets\/}"
         echo "----------------\n$INPUT -> $OUTPUT\n----------------\n"
         docker run --rm -v "$(pwd):/app" raeffs/glyphhanger --whitelist="${SUBSETCODES[i]}" --formats="${FORMATS[j]}" --subset="$INPUTPATH/${FILENAMES[k]}.$EXTENSION" --css
-        mkdir -p $(echo "$OUTPUTPATH")
-        mv $(echo $INPUT) $(echo $OUTPUT)
-        sed $(echo $REPLACE) $(echo "${INPUTPATH//\.\//}/${FILENAMES[k]}.css") | sed s/file/record/ >> $(echo "$OUTPUTPATH/../index.css")
-        echo "\n\n" >> $(echo "$OUTPUTPATH/../index.css")
-        rm $(echo "$INPUTPATH/${FILENAMES[k]}.css")
+        mkdir -p "$OUTPUTPATH"
+        mv $INPUT $OUTPUT
+        sed $REPLACE "${INPUTPATH//\.\//}/${FILENAMES[k]}.css" | sed s/file/record/ | sed "$FONT_FAMILY" | sed "${FONT_STYLE//Regular/normal}" | sed s/Bold/bold/ | sed s/Medium/medium/ >> "$OUTPUTPATH/../index.css"
+        echo "\n\n" >> "$OUTPUTPATH/../index.css"
+        rm "$INPUTPATH/${FILENAMES[k]}.css"
       done
     done
   done
