a9e7b341a80ac9bdc69c72b10703d6e6d32b3c2a
Igor Korovchenko <igsekor@gmail.com>
2023-11-13 19:46:59 +0300

    Правит скрипт до окончательного состояния

--- font.sh
+++ font.sh
@@ -1,15 +1,23 @@
 # Основной скрипт для генерации файлов шрифтов
 GENERATE_FONT() {
+  rm -f $(echo "$OUTPUTPATH/../index.css")
   for i in "${!SUBSETNAMES[@]}"; do
     for j in "${!FORMATS[@]}"; do
       for k in "${!FILENAMES[@]}"; do
         INPUT="$INPUTPATH/${FILENAMES[k]}-subset.${FORMATS[j]}"
         OUTPUT="$OUTPUTPATH/${FILENAMES[k]}-${SUBSETNAMES[i]}.${FORMATS[j]}"
+        REPLACE=$(echo "s/${INPUT//\//\\/}/${OUTPUT//\//\\/}/")
+        REPLACE=$(echo "${REPLACE//\.\\\//}")
+        REPLACE=$(echo "${REPLACE//\./\\\.}")
+        REPLACE=$(echo "${REPLACE//src\\\//\/}")
+        REPLACE=$(echo "${REPLACE//service\//assets\/}")
         echo "----------------\n$INPUT -> $OUTPUT\n----------------\n"
         docker run --rm -v "$(pwd):/app" raeffs/glyphhanger --whitelist="${SUBSETCODES[i]}" --formats="${FORMATS[j]}" --subset="$INPUTPATH/${FILENAMES[k]}.$EXTENSION" --css
         mkdir -p $(echo "$OUTPUTPATH")
-        mv $(echo "$INPUT") $(echo "$OUTPUT")
-        mv $(echo "$INPUTPATH/${FILENAMES[k]}.css") $(echo "$OUTPUTPATH")
+        mv $(echo $INPUT) $(echo $OUTPUT)
+        sed $(echo $REPLACE) $(echo "${INPUTPATH//\.\//}/${FILENAMES[k]}.css") | sed s/file/record/ >> $(echo "$OUTPUTPATH/../index.css")
+        echo "\n\n" >> $(echo "$OUTPUTPATH/../index.css")
+        rm $(echo "$INPUTPATH/${FILENAMES[k]}.css")
       done
     done
   done
@@ -17,10 +25,12 @@ GENERATE_FONT() {
 
 # Глобальные переменные
 # Набор названий для сабсетов
-SUBSETNAMES=("Latin" "LatinSupplement" "LatinExtendedA" "LatinExtendedB" "GreekCoptic" "Cyrilic" "CyrilicSupplement")
+# SUBSETNAMES=("Latin" "LatinSupplement" "LatinExtendedA" "LatinExtendedB" "GreekCoptic" "Cyrilic" "CyrilicSupplement")
+SUBSETNAMES=("Latin")
 
 # Набор диапазонов кодов глифов для каждого из сабсетов
-SUBSETCODES=("0000−007F" "0080−00FF" "0100−017F" "0180−024F" "0370−03FF" "0400−04FF" "0500−052F")
+# SUBSETCODES=("0000−007F" "0080−00FF" "0100−017F" "0180−024F" "0370−03FF" "0400−04FF" "0500−052F")
+SUBSETCODES=("0000−007F")
 FORMATS=("woff" "woff2")
 
 # Набор значений переменных для шрифта FiraSans
