8150c50df7e67b52b347fbd19fd6ab944a6cca5f
Igor Korovchenko <igsekor@gmail.com>
2023-11-15 00:50:32 +0300

    Правит скрипт

--- font.sh
+++ font.sh
@@ -11,13 +11,12 @@ GENERATE_FONT() {
         OUTPUT="$OUTPUTPATH/${FILENAMES[k]}-${SUBSETNAMES[i]}.${FORMATS[j]}"
         FONT_FAMILY="s/src/font\-family:\ ${FILENAMES[k]//\-*/};\n\ \ src/"
         FONT_STYLE="s/}/\ \ font-style: Regular;\n\ \ font-weight: ${FILENAMES[k]//*\-/};\n\ \ font-display: fallback;\n}/"
-        REPLACE="s!${INPUT//\.\/service/service}!${OUTPUT//\.\/src\/assets/\/assets}!"
+        REPLACE="s!${INPUT//\.\/service/service}!${OUTPUT//\.\/public\/assets/\/assets}!"
         echo "----------------\n$INPUT -> $OUTPUT\n----------------\n"
         docker run --rm -v "$(pwd):/app" raeffs/glyphhanger --whitelist="${SUBSETCODES[i]}" --formats="${FORMATS[j]}" --subset="$INPUTPATH/${FILENAMES[k]}.$EXTENSION" --css
         mkdir -p "$OUTPUTPATH"
         mv $INPUT $OUTPUT
-        echo $REPLACE
-        cat "${INPUTPATH//\.\//}/${FILENAMES[k]}.css" | sed "$REPLACE" | sed s/file/record/ | sed "$FONT_FAMILY" | sed -e "${FONT_STYLE//\:\ Regular/\:\ normal}" | sed s/\:\ Bold/\:\ bold/ | sed s/\:\ Medium/\:\ medium/ >> "$OUTPUTPATH/../index.css"
+        cat "${INPUTPATH//\.\//}/${FILENAMES[k]}.css" | sed "$REPLACE" | sed s/file/record/ | sed "$FONT_FAMILY" | sed -e "${FONT_STYLE//\:\ Regular/\:\ normal}" | sed s/\:\ Bold/\:\ bold/ | sed s/\:\ Medium/\:\ 500/ >> "$OUTPUTPATH/../index.css"
         echo "\n\n" >> "$OUTPUTPATH/../index.css"
         rm "$INPUTPATH/${FILENAMES[k]}.css"
       done
@@ -33,18 +32,18 @@ SUBSETNAMES=("Latin" "LatinSupplement" "LatinExtendedA" "LatinExtendedB" "GreekC
 
 # Набор диапазонов кодов глифов для каждого из сабсетов
 SUBSETCODES=("0000−007F" "0080−00FF" "0100−017F" "0180−024F" "0370−03FF" "0400−04FF" "0500−052F")
-FORMATS=("woff" "woff2")
+FORMATS=("woff2")
 
 # Набор значений переменных для шрифта FiraSans
 FILENAMES=("FiraSans-Bold" "FiraSans-Medium" "FiraSans-Regular")
 EXTENSION="ttf"
 INPUTPATH="./service/fonts/Fira_Sans"
-OUTPUTPATH="./src/assets/fonts/Fira-Sans"
+OUTPUTPATH="./public/assets/fonts/Fira-Sans"
 GENERATE_FONT
 
 # Набор значений переменных для шрифта FiraMono
 FILENAMES=("FiraMono-Medium" "FiraMono-Regular")
 EXTENSION="ttf"
 INPUTPATH="./service/fonts/Fira_Mono"
-OUTPUTPATH="./src/assets/fonts/Fira-Mono"
+OUTPUTPATH="./public/assets/fonts/Fira-Mono"
 GENERATE_FONT
