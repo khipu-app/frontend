0c2fcf77daf6022b0baa517c6f1fa8c6546f711a
Igor Korovchenko <igsekor@gmail.com>
2023-11-15 21:08:06 +0300

    Выносит логотип в отдельный компонент

---
+++ src/components/blocks/Logo.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { useSettingsStore } from '../../stores/settings'
+const { khipuLogo, khipuName } = useSettingsStore()
+</script>
+
+<template>
+  <div class="logo">
+    <img
+      class="logo__image"
+      alt="Логотип Kiphu"
+      :src="`/assets/images/${khipuLogo}.svg`"
+      width="150"
+      height="150"
+    >
+    <h1 class="logo__name typo typo--mono">{{ khipuName }}</h1>
+  </div>
+</template>
+
+<style>
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
+}
+
+.logo__image {
+  display: inherit;
+  width: 150px;
+  height: 150px;
+}
+
+.logo__name {
+  display: inherit;
+  color: var(--bg-panel, #000000);
+  font-family: var(--font-family);
+  font-size: var(--font-size-xxl);
+  font-weight: 500;
+  line-height: var(--font-line-height-xxl);
+  letter-spacing: var(--letter-spacing);
+}
+</style>
--- src/components/views/Launch.vue
+++ src/components/views/Launch.vue
@@ -2,24 +2,19 @@
 import { useRouter } from 'vue-router'
 import { useSettingsStore } from '../../stores/settings'
 
-const settings = useSettingsStore()
+import Logo from '../blocks/Logo.vue'
+
+const { khipuLaunchTimer } = useSettingsStore()
 
 const router = useRouter()
 setTimeout(() => {
   router.push({ name: 'Library' })
-}, settings.khipuLaunchTimer);
+}, khipuLaunchTimer);
 </script>
 
 <template>
   <div class="launch">
-    <img
-      class="launch__logo"
-      alt="Логотип Kiphu"
-      :src="`/assets/images/${settings.khipuLogo}.svg`"
-      width="150"
-      height="150"
-    >
-    <h1 class="launch__name typo typo--mono">Khipu</h1>
+    <Logo class="launch__logo" />
   </div>
 </template>
 
@@ -29,25 +24,10 @@ setTimeout(() => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 20px;
-  margin: 0;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
   background-color: var(--bg-tone-gold);
 }
 
 .launch__logo {
   display: inherit;
-}
-
-.launch__name {
-  display: inherit;
-  color: var(--bg-panel, #000000);
-  font-family: var(--font-family);
-  font-size: var(--font-size-xxl);
-  font-weight: 500;
-  line-height: var(--font-line-height-xxl);
-  letter-spacing: var(--letter-spacing);
 }
 </style>
