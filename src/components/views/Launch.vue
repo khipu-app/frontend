967b99c709cc566f2dcda712d6ac86be741101dd
Igor Korovchenko <igsekor@gmail.com>
2023-11-15 19:46:50 +0300

    Добавляет экран загрузки

---
+++ src/components/views/Launch.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+import { ref } from 'vue'
+
+let logo = ref('logo-default')
+const logoFromSettings = localStorage.getItem('khipu-logo')
+if (logoFromSettings) {
+  logo = logoFromSettings
+}
+
+let launchTime = 1000 // ms
+const launchTimeFromSettings = localStorage.getItem('khipu-logo')
+if (logoFromSettings) {
+  launchTime = launchTimeFromSettings
+}
+
+const router = useRouter()
+setTimeout(() => {
+  router.push({ name: 'Library' })
+}, launchTime);
+</script>
+
+<template>
+  <div class="launch">
+    <img
+      class="launch__logo"
+      alt="Логотип Kiphu"
+      :src="`/assets/images/${logo}.svg`"
+      width="150"
+      height="150"
+    >
+    <h1 class="launch__name typo typo--mono">Khipu</h1>
+  </div>
+</template>
+
+<style>
+.launch {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--bg-tone-gold);
+}
+
+.launch__logo {
+  display: inherit;
+}
+
+.launch__name {
+  display: inherit;
+  color: var(--bg-panel, #000000);
+  font-family: var(--font-family);
+  font-size: var(--font-size-xxl);
+  font-weight: 500;
+  line-height: var(--font-line-height-xxl);
+  letter-spacing: var(--letter-spacing);
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Library from './components/views/Library.vue'
+import Launch from './components/views/Launch.vue'
 
 const routes = [
+  { path: '/', name: 'Launch', component: Launch },
   { path: '/library', name: 'Library', component: Library },
 ]
 
